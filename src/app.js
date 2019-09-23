268b39ec84a70b3ba50e15ea8684ae2516fb19f2
willemodendaal <willem@newmedialabs.co.za>
2019-09-23 15:19:32 +0200

    Added header-footer-toolbar layout.

--- src/app.js
+++ src/app.js
@@ -5,10 +5,19 @@ import "./styles/main.scss";
 const App = () => {
     return (
         <div className={"container"}>
-            <div>Item1</div>
-            <div>Item2</div>
-            <div>Item3</div>
-            <div>Item4</div>
+            <div id={"header"}>
+                <div id={"logo"} >[logo]</div>
+                <div id={"header-links"}>
+                    <a href>About</a>
+                    <a href>Guide</a>
+                </div>
+            </div>
+            
+            <div id={"toolbar"}>Toolbar</div>
+            
+            <div id={"main"}>Main Body</div>
+            
+            <div id={"footer"}>Footer</div>
         </div>
     );
 };

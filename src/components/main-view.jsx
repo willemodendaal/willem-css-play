0ed87e575d91c4a5e36dfda7399c4274da23176b
willemodendaal <willem@newmedialabs.co.za>
2019-09-23 15:56:27 +0200

    Added stage-items.

--- src/components/main-view.jsx
+++ src/components/main-view.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const MainView = () => {
+    const stageItems = [...Array(50)].map(() => <div className={"stage-item"}></div>);
+
     return (
         <div id={"main-view"}>
-            <div id={"stage"}>Stage</div>
+            <div id={"stage"}>
+                {stageItems}
+            </div>
             <div id={"stage-filters"}>Filters</div>
         </div>
     );
